fb2f9a50cb436b24e32a39e4f71b3fc1af715e6b
vl-tr <lmac@mail.ru>
2021-02-11 17:03:03 +0300

    final changes

--- app/src/main/java/com/tvastudio/musicshop/OrderActivity.java
+++ app/src/main/java/com/tvastudio/musicshop/OrderActivity.java
@@ -3,18 +3,24 @@ package com.tvastudio.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    String[] addreses = {"lmac@mail.ru"};
+    String subject = "Order from Music Shop";
+    String emailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
-        getSupportActionBar().setTitle("Your Order");
+        setTitle("Your order");
 
         Intent receivedOrderIntent = getIntent();
         String userName = receivedOrderIntent.getStringExtra("userName");
@@ -23,9 +29,23 @@ public class OrderActivity extends AppCompatActivity {
         double goodsPrice = receivedOrderIntent.getDoubleExtra("goodsPrice", 0);
         double orderPrice = receivedOrderIntent.getDoubleExtra("orderPrice", 0);
 
+        emailText = "Customer name: "+ userName +"\n"+ "Goods name: " + goodsName +
+                "\n"+ "Quantity: " + quantity +"\n" +"Price: " + goodsPrice + "\n" +
+                "Order Price: " + orderPrice;
+
         TextView orderTextView = findViewById(R.id.orderTextView);
-        orderTextView.setText("Customer name: "+ userName +"\n"+ "Goods name: " + goodsName +
-                               "\n"+ "Quantity: " + quantity +"\n" +"Price: " + goodsPrice + "\n" +
-                               "Order Price: " + orderPrice);
+        orderTextView.setText(emailText);
+    }
+
+    public void submitOrder(View view) {
+        //Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        //emailIntent.setData(Uri.parse("mailto:"));
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("*/*");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, addreses);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, emailText);
+        startActivity(emailIntent);
+
     }
 }
